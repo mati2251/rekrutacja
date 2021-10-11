b5baa3cb2b51f13af5870a2461b223655b3cbb71
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2021-10-11 09:26:06 +0200

    Riddles done

--- js/riddles/index.js
+++ js/riddles/index.js
@@ -1,7 +1,7 @@
 // 1. odwróć liczbę
 // np dla 12345, funkcja powinna zwrócić 54321
-function reverseNumber() {
-  // uzupełnij tutaj
+function reverseNumber(number) {
+  return number.toString().split('').reverse().join('')
 }
 
 console.log("1.", reverseNumber(12345));
@@ -10,7 +10,13 @@ console.log("1.", reverseNumber(12345));
 // dla tablicy tab powinniśmy otrzymać 2 + 4 + 6 + 8 = 20
 const tab = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 function addEven(array) {
-  // uzupełnij tutaj
+  let sum = 0
+  array.forEach((element) => {
+    if(element%2 === 0){
+      sum += element
+    }
+  })
+  return sum;
 }
 
 console.log("2.", addEven(tab));
