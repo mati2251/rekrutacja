a1299905b305009e36446924a4b66603a39bca4d
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2021-10-12 00:29:54 +0200

    html-css-js done

--- html-css-js/js/validation.js
+++ html-css-js/js/validation.js
@@ -1,11 +1,22 @@
-/*
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    AKAI Frontend Task - Javascript
-
-    W tym zadaniu postaraj się zaimplementować metody, które sprawdzą, czy dane wprowadzone
-    do formularza są poprawne. Przykładowo: czy imię i nazwisko zostało wprowadzone.
-    Możesz rozwinąć walidację danych tak bardzo, jak tylko zapragniesz.
-
-    Powodzenia!
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-*/
+const validData = () => {
+  let isValid = true;
+  const firstName = document.getElementById('first-name').value
+  if (!/^[a-z ,.'-]+$/i.test(firstName)) {
+    isValid = false;
+  }
+  const secondName = document.getElementById('last-name').value;
+  if (!/^[a-z ,.'-]+$/i.test(secondName)) {
+    isValid = false;
+  }
+  const email = document.getElementById('email').value;
+  if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+    isValid = false;
+  }
+  if(!isValid){
+    document.getElementById('alert').innerText = 'Złe dane!!!'
+    return false;
+  }
+  else {
+    document.getElementById('alert').innerText = ''
+  }
+}
